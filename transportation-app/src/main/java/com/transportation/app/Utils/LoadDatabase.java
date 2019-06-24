70249ed4eb4495c7a384dfa30ec62a8dc4312df9
Varvara Tzika <varvaratzika@Varvaras-MacBook.local>
2019-06-25 00:13:26 +0200

    parsing and reading file

--- transportation-app/src/main/java/com/transportation/app/Utils/LoadDatabase.java
+++ transportation-app/src/main/java/com/transportation/app/Utils/LoadDatabase.java
@@ -2,20 +2,29 @@ package com.transportation.app;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.InputStream;
+import java.io.IOException;
+import java.lang.Exception;
+import java.util.Iterator;
+import java.io.File;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 @Configuration
 class LoadDatabase {
-	
+
 	@Bean
 	CommandLineRunner initDatabase(JourneyRepository repository) {
 		return args -> {
-			
+
 			System.out.println("loading journey data...");
 			List<Journey> journeys = new ArrayList<Journey>();
 			Journey j1 = new Journey();
@@ -25,8 +34,26 @@ class LoadDatabase {
 			j1.setDeparture_stop_name("Den Dungen, Dungens Molen");
 			j1.setArrival_stop_name("Den Bosch");
 			repository.save(j1);
+
+			readDataFromJSON();
 		};
 	}
+
+	public void readDataFromJSON() {
+		JSONParser parser = new JSONParser();
+		try {
+ 
+			File myFile = new File("testData.json");
+			System.out.println("Attempting to read from file in: " + myFile.getCanonicalPath());
+			Object obj = parser.parse(new FileReader(myFile.getCanonicalPath()));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			JSONObject initial = (JSONObject) jsonObject.get("63150050");
+			System.out.println("initial: " + initial);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-
--- transportation-app/src/main/java/com/transportation/app/controllers/JourneyController.java
+++ transportation-app/src/main/java/com/transportation/app/controllers/JourneyController.java
@@ -23,5 +23,5 @@ public class JourneyController {
     public Iterable<Journey> list() {
         return journeyService.list();
     }
-    
+  
 }
